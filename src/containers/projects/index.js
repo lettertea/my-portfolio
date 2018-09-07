6c70f5543badd7fd1df499b9a7d5eba3de921333
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-09-07 11:14:46 -0700

    Add introduction

--- src/containers/projects/index.js
+++ src/containers/projects/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import projects from "./projects";
-import "./styles.css"
+import "./styles.css";
 
-const Row = ({title, type, description, source, score}) => (
+const Row = ({ title, type, description, source, score }) => (
   <tr id="data-row">
     <td>{title}</td>
     <td>{type}</td>
     <td>{description}</td>
-    <td><a href={source} target="_blank" rel="noopener noreferrer">{source}</a></td>    
-    <td>{score}</td>    
+    <td>
+      <a href={source} target="_blank" rel="noopener noreferrer">
+        {source}
+      </a>
+    </td>
+    <td>{score}</td>
   </tr>
 );
 
@@ -16,15 +20,15 @@ class Projects extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: projects,
+      data: projects
     };
-    
+
     this.compareBy.bind(this);
     this.sortBy.bind(this);
   }
-  
+
   compareBy(key) {
-    return function (a, b) {
+    return function(a, b) {
       if (a[key] < b[key]) return -1;
       if (a[key] > b[key]) return 1;
       return 0;
@@ -32,42 +36,50 @@ class Projects extends React.Component {
   }
 
   reverse(key) {
-    return function (a, b) {
+    return function(a, b) {
       if (a[key] > b[key]) return -1;
       if (a[key] < b[key]) return 1;
       return 0;
     };
   }
- 
+
   sortBy(key) {
     let arrayCopy = [...this.state.data];
     if (this.state.recentlySorted === key) {
       console.log("hi");
       arrayCopy.reverse();
     } else {
-    arrayCopy.sort(this.compareBy(key));
+      arrayCopy.sort(this.compareBy(key));
+    }
+    this.setState({ data: arrayCopy, recentlySorted: key });
   }
-    this.setState({data: arrayCopy, recentlySorted: key});
-  }
-    
+
   render() {
-    const rows = this.state.data.map( (rowData) => <Row {...rowData} />);
+    const rows = this.state.data.map(rowData => <Row {...rowData} />);
 
     return (
-      <div id="table-container">
-      <table>
-        <tr id="header">
-          <th onClick={() => this.sortBy('title')} >Title</th>
-          <th onClick={() => this.sortBy('type')}>Type</th>
-          <th onClick={() => this.sortBy('description')}>Description</th>
-          <th onClick={() => this.sortBy('source')}>Source</th>
-          <th onClick={() => this.sortBy('score')}>Pride Score</th>
-        </tr>
-          {rows}
-      </table>
+      <div>
+        <h2>Introduction</h2>
+        <div id="intro">
+          I'm a CS student named Thomas that tries to do web development. I've
+          been doing my projects purely on the front-end recently, so I'm
+          thinking about branching out to the back-end.
+        </div>
+        <h2>Projects</h2>
+        <div id="table-container">
+          <table>
+            <tr id="header">
+              <th onClick={() => this.sortBy("title")}>Title</th>
+              <th onClick={() => this.sortBy("type")}>Type</th>
+              <th onClick={() => this.sortBy("description")}>Description</th>
+              <th onClick={() => this.sortBy("source")}>Source</th>
+              <th onClick={() => this.sortBy("score")}>Pride Score</th>
+            </tr>
+            {rows}
+          </table>
+        </div>
       </div>
     );
-    
   }
 }
 
