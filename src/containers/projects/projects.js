9bfe91f0a34ffb81929ff5e8c265f8416cfac166
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2018-09-07 20:34:03 -0700

    Add more projects and edit descriptions

--- src/containers/projects/projects.js
+++ src/containers/projects/projects.js
@@ -2,7 +2,8 @@ export default [
   {
     title: "React Visual Novel",
     type: "Web Application",
-    description: "Renders sounds, images, and user choices to provide an interactive story, or visual novel, via React.",
+    description:
+      "Renders sounds, images, and user choices to provide an interactive story, or visual novel, via React.",
     source: "https://github.com/nashkenazy/React-Visual-Novel",
     score: 9
   },
@@ -10,7 +11,7 @@ export default [
     title: "Classes Search Aid",
     type: "Web Extension",
     description:
-      "Shows professor ratings directly from Rate My Professor in CSULB, CSULA, and UC Merced's class search (in their respective branches) and creates a colored guide on which professor is most compatible.",
+      "Checks schedule compatibility with classes, appends Rate My Professor ratings and review count alongside the professor's slot, and color codes it accordingly.",
     source: "https://github.com/nashkenazy/classes-search-aid",
     score: 8
   },
@@ -18,16 +19,55 @@ export default [
     title: "MEAN Blogs",
     type: "Web Application",
     description:
-      "A social media clone where the user may view their own profile with their blogs, other user profiles, or the public blog feed with all blogs. Each blog includes likes, dislikes, and comments functionalities.",
+      "A social media clone with posts, comments, likes, dislikes, and profiles.",
     source: "https://github.com/nashkenazy/MEAN-Blog",
     score: 6.5
+  },
+  {
+    title: "Portfolio Site (deprecated)",
+    type: "Web Application",
+    description:
+      "Shows some of my projects with overdone CSS",
+    source: "https://github.com/nashkenazy/portfolio-site",
+    score: 6
   },
   {
     title: "Django RPS",
     type: "Web Application",
     description:
-      'A user authenticated application where the results of playing "Rock, Paper, Scissors" against the computer are automatically tallied and stored to the database. Each user also has a profile page that only they can view and edit.',
+      'Challenges the user to a game of "Rock, Paper, Scissors." The user can register for an account to store results and make a profile.',
     source: "https://github.com/nashkenazy/Django-RPS",
     score: 5.5
+  },
+  {
+    title: "Some Django Blog",
+    type: "Web Application",
+    description: "A personal blog where users can view my blog posts.",
+    source: "https://github.com/nashkenazy/Some-Django-Blog",
+    score: 5.5
+  },
+  {
+    title: "Password Strength Checker",
+    type: "Web Application",
+    description:
+      "Allows the user to register and login to the server. During signup, the password's strength will be tested against Dropbox's zxcvbn library.",
+    source: "https://github.com/nashkenazy/Password-Strength-Checker",
+    score: 4.5
+  },
+  {
+    title: "React Runner Quiz",
+    type: "Web Application",
+    description: "A five question quiz that guesses what type of runner the user is.",
+    source: "https://github.com/nashkenazy/React-Runner-Quiz",
+    score: 4
+  },
+
+  {
+    title: "React Todo",
+    type: "Web Application",
+    description:
+      'An introductory CRUD application without the U.',
+    source: "https://github.com/nashkenazy/React-Project-Manager",
+    score: 2
   }
 ];
